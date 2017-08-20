d7e8efc6aaf02223e70844003d8ec41b805c44b8
John Doe <nb4win10@i.ua>
2017-08-20 19:56:21 +0300

    file Permission.java is changed: alowed -> allowed

--- projectresources/src/main/java/com/softserve/edu/resources/entities/Permissions.java
+++ projectresources/src/main/java/com/softserve/edu/resources/entities/Permissions.java
@@ -1,44 +1,47 @@
+/*
+ * 
+ */
 package com.softserve.edu.resources.entities;
 
 public class Permissions {
 
-    private int id;
-    private int id_role;
-    private int id_feature;
-    private String alowed;
+	private int id;
+	private int id_role;
+	private int id_feature;
+	private String allowed;
 
-    public Permissions() {
-    }
+	public Permissions() {
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public int getId_role() {
-        return id_role;
-    }
+	public int getId_role() {
+		return id_role;
+	}
 
-    public void setId_role(int id_role) {
-        this.id_role = id_role;
-    }
+	public void setId_role(int id_role) {
+		this.id_role = id_role;
+	}
 
-    public int getId_feature() {
-        return id_feature;
-    }
+	public int getId_feature() {
+		return id_feature;
+	}
 
-    public void setId_feature(int id_feature) {
-        this.id_feature = id_feature;
-    }
+	public void setId_feature(int id_feature) {
+		this.id_feature = id_feature;
+	}
 
-    public String getAlowed() {
-        return alowed;
-    }
+	public String getAllowed() {
+		return allowed;
+	}
 
-    public void setAlowed(String alowed) {
-        this.alowed = alowed;
-    }
+	public void setAllowed(String allowed) {
+		this.allowed = allowed;
+	}
 }
