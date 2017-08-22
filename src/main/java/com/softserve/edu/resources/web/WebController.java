98dca827f3da688f3f5d34b70bea87468e9ddda0
bebebe email = hahaha@gmail.com <yharasym@CLASS02.training.local>
2017-08-22 17:26:59 +0300

    Spring Web MVC with Static Pages

--- src/main/java/com/softserve/edu/resources/web/WebController.java
+++ src/main/java/com/softserve/edu/resources/web/WebController.java
@@ -1,19 +1,31 @@
 package com.softserve.edu.resources.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebController {
     
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
 
-    @RequestMapping(value = "/final", method = RequestMethod.GET)
-    public String redirect() {
-        return "redirect:/static/html/final.htm";
-    }
+  @RequestMapping(value = "/{page}.html", method = RequestMethod.GET)
+  public String redirect(@PathVariable("page") String page) {
+      return "redirect:/static/html/" + page + ".html";
+  }
+
+//    @RequestMapping(value = "/final", method = RequestMethod.GET)
+//    public String redirect() {
+//        return "redirect:/static/html/final.htm";
+//    }
+
 }
