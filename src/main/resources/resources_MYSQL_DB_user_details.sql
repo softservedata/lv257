fa1f6e4244946cb6cc81b566f2f5b920c4879cd0
Andriy Lyapandra <lyapandra.andriy.s@gmail.com>
2017-09-19 15:09:29 +0300

    add resources_MYSQL_DB_user_details.sql for fill(1 row) user_details table

---
+++ src/main/resources/resources_MYSQL_DB_user_details.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_details (Id,First_Name,Second_Name,Middle_Name,Passport_Series,Passport_Number,Issued_By,Date_Of_Issue,Id_Address,Phone,Bank_id)
+ VALUES (7,'First_Name7','Second_Name7','Middle_Name7','AB',777777,'ADMIN_7','2017-07-7','7','(380)-12-34-57','777777777');
