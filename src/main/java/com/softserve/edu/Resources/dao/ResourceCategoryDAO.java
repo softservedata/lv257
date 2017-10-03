b3264fe670e7f912447872f90b53082928b21062
Andriy Lyapandra <lyapandra.andriy.s@gmail.com>
2017-10-03 11:27:10 +0300

    delete old version of project from devAndriy (prepare for upload new version)

--- src/main/java/com/softserve/edu/Resources/dao/ResourceCategoryDAO.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.edu.Resources.dao;
-
-import com.softserve.edu.Resources.entity.ResourceCategory;
-
-public interface ResourceCategoryDAO extends GenericDAO<ResourceCategory, Long> {
-
-    ResourceCategory findByName(String name);
-}
--- src/main/java/com/softserve/edu/Resources/dao/ResourceTypeDAO.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.edu.Resources.dao;
-
-import com.softserve.edu.Resources.entity.ResourceType;
-
-public interface ResourceTypeDAO extends GenericDAO<ResourceType, Long> {
-
-    ResourceType findByName(String name);
-}
