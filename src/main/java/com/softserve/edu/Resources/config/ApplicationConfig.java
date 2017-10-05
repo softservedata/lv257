0d7b0b88289eb5fe6869bc6b57be5892f223ed01
IraVasylyk13 <ira.vasylyk13@meta.ua>
2017-10-05 13:21:17 +0300

    changing bean creation

--- src/main/java/com/softserve/edu/Resources/config/ApplicationConfig.java
+++ src/main/java/com/softserve/edu/Resources/config/ApplicationConfig.java
@@ -21,12 +21,12 @@ import java.util.Properties;
 @EnableTransactionManagement
 @Configuration
 @Import(DBConfig.class)
-@ComponentScan(basePackages={"com.softserve.edu.Resources"})
+@ComponentScan(basePackages = {"com.softserve.edu.Resources"})
 @PropertySource("classpath:mail.properties")
 public class ApplicationConfig {
 
     @Bean
-    public MessageSource messageSource(){
+    public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
@@ -47,13 +47,13 @@ public class ApplicationConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
         mailSender.setHost(env.getProperty("mail.host"));
-        mailSender.setPort(env.getProperty("mail.port",Integer.class));
+        mailSender.setPort(env.getProperty("mail.port", Integer.class));
         mailSender.setUsername(env.getProperty("mail.username"));
         mailSender.setPassword(env.getProperty("mail.password"));
 
         Properties props = mailSender.getJavaMailProperties();
 
-        props.put("mail.transport.protocol",env.getProperty("mail.transport.protocol"));
+        props.put("mail.transport.protocol", env.getProperty("mail.transport.protocol"));
         props.put("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
         props.put("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable"));
         props.put("mail.debug", env.getProperty("mail.debug"));
@@ -62,24 +62,13 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public VelocityEngine getVelocityEngine()throws IOException{
-//
-//        Properties properties=new Properties();
-//        properties.setProperty(VelocityEngine.RESOURCE_LOADER, env.getProperty("resource.loader"));
-//        properties.setProperty("classpath." + VelocityEngine.RESOURCE_LOADER + ".class",env.getProperty("class.resource.loader.class"));
-//        Velocity.init(properties);
-//        VelocityEngine velocityEngine=new VelocityEngine();
-        // return velocityEngine;
+    public VelocityEngine getVelocityEngine(){
 
-        VelocityEngineFactory factory = new VelocityEngineFactory();
-        Properties props = new Properties();
-        props.put("resource.loader",  env.getProperty("velocity.resource.loader"));
-        props.put("class.resource.loader.class", env.getProperty("velocity.class.resource.loader.class"));
-        factory.setVelocityProperties(props);
-        return factory.createVelocityEngine();
-
-
-
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.setProperty("resource.loader", env.getProperty("velocity.resource.loader"));
+        velocityEngine.setProperty("class.resource.loader.class", env.getProperty("velocity.class.resource.loader.class"));
+        velocityEngine.init();
+        return velocityEngine;
     }
 
 }
