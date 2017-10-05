d8ab7ff81d97df2b568c77f1bce6e1095f81f862
RomanKhomyshynets <khom.rv@gmail.com>
2017-10-05 07:01:36 +0300

    Some changes in JS code

--- src/main/webapp/resources/js/categories.js
+++ src/main/webapp/resources/js/categories.js
@@ -5,15 +5,16 @@
  */
 
 (function () {
+    var includeTypes = (typeof showTypesInCategoryHierarchy == 'undefined') ? false : showTypesInCategoryHierarchy;
+    var suppressChoosingParents = (typeof disableAncestorSelecting == 'undefined') ? true : disableAncestorSelecting;
     var lastId;
-    var jsonForSelect;
 
     //Enable categories selectlist
-    loadCategories();
+    loadCategories(includeTypes, suppressChoosingParents);
 
     $('#manage-categories').on('click', function (e) {
         e.preventDefault();
-        buildCategoriesTree();
+        buildCategoriesNestableTree();
     });
 
     $('#save-changes').on('click', function (e) {
@@ -21,13 +22,13 @@
         saveChangesAfterManaging();
     });
 
-     //Discard changes and close modal window with categories managing
+    //Discard changes and close modal window with categories managing
     $('#cancel-managing, #close-managing').on('click', function (e) {
         e.preventDefault();
         $('#nestable').nestable('destroy');
     });
 
-     //Actions of menu buttons
+    //Actions of menu buttons
     $('#nestable-menu').on('click', function (e) {
         var target = $(e.target),
             action = target.data('action');
@@ -40,7 +41,7 @@
         if (action === 'add-item') {
             var newItem = {
 //                    "id": ++lastId,
-                "categoryname"  : "new category",
+                "categoryname": "new category",
 //                    "`nt_id" : 1516,
                 "content": "Item " + lastId
             };
@@ -55,33 +56,38 @@
     });
 
     /**
-     * Enable categories selectlist
+     * Load data and build categories selectlist
      */
     function loadCategories() {
-        var includeTypes = (typeof showTypesInCategoryHierarchy == 'undefined') ? false : showTypesInCategoryHierarchy;
-        var suppressParents = (typeof disableAncestorSelecting == 'undefined') ? true : disableAncestorSelecting;
         let urlSuffix = includeTypes ? 'categorizedTypes' : 'categories';
         $.get("/resources/" + urlSuffix, function (data) {
-            jsonForSelect = sortNestedComponents(data, 'categoryname', 'asc');
-            console.log(JSON.stringify(jsonForSelect));
-            BuildCategoriesSelect(jsonForSelect, includeTypes, suppressParents);
-            addHandlers();
-            $('#categories-select').hierarchySelect({
-                width: '100%',
-                height: '208px',
-                hierarchy: includeTypes,
-                search: true
-            });
+            showCategoriesSelect(data);
+        }, "json");
+    }
+
+    /**
+     * Reload data, rebuild categories selectlist and reselect item
+     * which was selected before reloading (if it still exists)
+     * @param lastSelectedId - ID of last selected item
+     */
+    function reloadCategories(lastSelectedId) {
+        let urlSuffix = includeTypes ? 'categorizedTypes' : 'categories';
+        $.get("/resources/" + urlSuffix, function (data) {
+            showCategoriesSelect(data);
+            var isLastSelectedItemExists = $('[data-value="'+lastSelectedId+'"] > a').length;
+            if(isLastSelectedItemExists) {
+                $('[data-value="'+lastSelectedId+'"] > a').click();
+            } //else TODO
         }, "json");
     }
 
     /**
      * Activate Nestable and build categories tree
      */
-    function buildCategoriesTree() {
+    function buildCategoriesNestableTree() {
         var jqxhr = $.getJSON("/resources/categories")
             .success(function (data) {
-                data = sortNestedComponents(data, 'categoryname', 'asc');
+                data = sortNestedComponents(data, 'categoryname', '', 'asc');
                 var json = JSON.stringify(data);
                 // activate Nestable
                 $('#nestable').nestable({
@@ -125,13 +131,15 @@
         $.ajax({
             type: "POST",
             contentType: "application/json",
-            url: "/resources/manageCategories",
+            url: "/resources/categories",
             accept: "application/json",
             data: json,
             success: function (result) {
                 alert("Changes are saved!");
-                jsonForSelect = json;
                 $('#close-managing').click();
+                let lastSelectedId = $('#categories_and_types').data('categoryID');
+                $('#categories_and_types').empty();
+                reloadCategories(lastSelectedId);
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 alert("jqXHR: " + jqXHR.status + " Status: " + textStatus + " Error: " + errorThrown);
@@ -162,72 +170,85 @@
     /**
      * Sort array of objects which may have nested objects by some object key
      * @param data - array of objects
-     * @param key - object key for sorting
+     * @param categoryKey - object key for sorting categories
+     * @param typeKey - object key for sorting types
      * @param way - ascending or descending order
      * @returns {Array.<T>} - array of sorted objects
      */
-    function sortNestedComponents(data, key, way) {
-        data = data.sort(function (a, b) {
-            var x = a[key];
-            var y = b[key];
-            if (way === 'asc') {
-                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-            }
-            if (way === 'desc') {
-                return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-            }
-        });
+    function sortNestedComponents(data, categoryKey, typeKey, way) {
+        data = sortComponents(data, categoryKey, way);
         for (var i = 0; i < data.length; i++) {
             if (data[i].children && data[i].children.length > 0) {
-                sortNestedComponents(data[i].children, key, way);
+                sortNestedComponents(data[i].children, categoryKey, typeKey, way);
+            }
+            var restypes = eval(data[i].restypes);
+            if (restypes && restypes.length > 0) {
+                restypes = sortComponents(restypes, typeKey, way);
             }
         }
         return data;
     }
 
     /**
-     *
+     * Build items of categories select
      * @param categories - JSON with categories and types
      * @param showTypes - include resource types in select
-     * @param suppressParentCategories - allow only leaf categories in select
+     * @param suppressChoosingParents - allow choosing only leaf categories in select
      * @param level - depth of hierarchy
      * @constructor
      */
-    function BuildCategoriesSelect(categories, showTypes, suppressParentCategories, level = 1) {
+    function BuildCategoriesSelect(categories, showTypes, suppressChoosingParents, level = 1) {
         for (var i = 0; i < categories.length; i++) {
-            categories = sortComponents(categories, 'categoryname', 'asc');
-            let classPrefix = (showTypes || (suppressParentCategories && categories[i].children)) ? 'disabled' : '';
+            let classPrefix = (showTypes || (suppressChoosingParents && categories[i].children)) ? 'disabled' : '';
             let s = $('<li/>', {
                 'data-value': categories[i].id,
                 'data-level': level,
                 class: classPrefix + ' category ' + 'level-' + level,
             }).appendTo($('#categories_and_types'));
-            $('<a/>',{ href:"#", text: categories[i].categoryname}).appendTo(s);
+            $('<a/>', {href: "#", text: categories[i].categoryname}).appendTo(s);
             if (showTypes) {
                 var restypes = eval(categories[i].restypes);
                 if (restypes) {
-                    restypes = sortComponents(restypes, 'typeName', 'asc');
                     for (var j = 0; j < restypes.length; j++) {
-                        let s1 =  $('<li/>', {
+                        let s1 = $('<li/>', {
                             'data-value': restypes[j].id,
-                            'data-level': level+1,
-                            class: 'level-' + level+1,
+                            'data-level': level + 1,
+                            class: 'level-' + level + 1,
                         }).appendTo($('#categories_and_types'));
-                        $('<a/>',{ href:"#", text: restypes[j].typeName}).appendTo(s1);
+                        $('<a/>', {href: "#", text: restypes[j].typeName}).appendTo(s1);
 
                     }
                 }
             }
             if (categories[i].children && categories[i].children.length > 0)
-                BuildCategoriesSelect(categories[i].children, showTypes, suppressParentCategories, level + 1);
+                BuildCategoriesSelect(categories[i].children, showTypes, suppressChoosingParents, level + 1);
         }
+    }
+
+    /**
+     * Show/repaint categories select on the page
+     * @param data - JSON with categories and types
+     * @param includeTypes - include resource types in select
+     * @param suppressChoosingParents - allow choosing only leaf categories in select
+     */
+    function showCategoriesSelect(data) {
+        data = sortNestedComponents(data, 'categoryname', 'typeName', 'asc');
+        console.log(JSON.stringify(data));
+        BuildCategoriesSelect(data, includeTypes, suppressChoosingParents);
+        addHandlers();
+        $('#categories-select').hierarchySelect({
+            width: '100%',
+            height: '208px',
+            hierarchy: includeTypes,
+            search: true
+        });
     }
 
     /**
      * Add event handler for clicking on selected item
      */
     function addHandlers() {
-        let list = $('#categories_and_types li a');
+        let list = $('#categories_and_types').find('li a');
         $.each(list, function (i, item) {
             $(item).click(function (e) {
                 $('#categories_and_types').data('categoryID', $(e.target).closest('li').data('value'));
