198f69d11f5feacb7f7797e9449e3313b9e75efa
OlehTsebak <oleg.tsebak16@gmail.com>
2017-10-31 01:55:43 +0200

    Added methods to persist recource entity, recource ownings entity, and concrete recource record (plain sql for now).

--- src/main/java/com/softserve/edu/Resources/service/ResourceService.java
+++ src/main/java/com/softserve/edu/Resources/service/ResourceService.java
@@ -2,16 +2,13 @@ package com.softserve.edu.Resources.service;
 
 import com.softserve.edu.Resources.dto.GenericResourceDTO;
 import com.softserve.edu.Resources.dto.GroupedResourceCount;
+import com.softserve.edu.Resources.dto.ResourceImplDTO;
 import com.softserve.edu.Resources.entity.GenericResource;
-import com.softserve.edu.Resources.entity.PropertyValue;
+import com.softserve.edu.Resources.entity.Resource;
 import com.softserve.edu.Resources.entity.ResourceType;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ResourceService {
 
@@ -20,7 +17,13 @@ public interface ResourceService {
 //    Collection<ResourceType> getInstantiatedResourceTypes();
 //
 //    GenericResource addResource(ResourceType resourceType, Set<PropertyValue> resourceValues);
-    
+
+    void addResource(Resource resource);
+
+    void addResourceOwnings(Resource resource, ResourceImplDTO resourceImplDTO);
+
+    public void addResourceImpl(Resource resource, ResourceType resourceType, Map<String, String> propertiesAndValues);
+
     List<GenericResource> findResourcesByResourceType(GenericResourceDTO genericResourceDTO);
     
     List<GroupedResourceCount> findResourcesCountGroupedByResourceTypeForOwner(String ownerId);
